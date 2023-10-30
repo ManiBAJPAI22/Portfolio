22d22bdbc26747018241debb9d48ef7224368d7e
Mani_B <manicode22@gmail.com>
2023-10-31 04:49:39 +0530

    First Commit

---
+++ components/about.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        Currently pursuing a degree in{" "}
+        <span className="font-medium">
+          Electronics and Communication Engineering
+        </span>
+        , I decided to pursue my passion for programming. I enrolled myself in a
+        coding bootcamp and learned{" "}
+        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack
+        is{" "}
+        <span className="font-medium">
+          React, Next.js, Node.js, Javascript and MongoDB
+        </span>
+        . I am also familiar with TypeScript and Prisma. I am always looking to
+        learn new technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer.
+      </p>
+
+      <p>
+        <span className="italic">Outside of coding</span>, I find enjoyment in
+        playing video games, watching movies, and working out at the gym. I also
+        find pleasure in{" "}
+        <span className="font-medium">acquiring new knowledge</span>. At
+        presently, I am delving into the realm of{" "}
+        <span className="font-medium">Microprocessors & Controllers</span>.
+        These subjects are highly intriguing to me.
+      </p>
+    </motion.section>
+  );
+}
